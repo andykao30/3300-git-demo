2236c0c04543878a74924c1cd6195010acc99a1f
andykao30 <andykao@gatech.edu>
2023-08-29 15:22:42 -0400

    updates

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("demo demo demo");
+        System.out.println("hi");
     }
 }
--- newnewbranch.java
+++ newnewbranch.java
@@ -1,6 +1,7 @@
 public class newnewbranch {
     public static void main(String[] args) {
-        int x = 3;
+        int x = 10;
         System.out.println(x);
+        int y = 23;
     }
 }
